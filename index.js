af63bd63f25525becba10fbda261f0a5b2dd9907
sherlyls <sherlyls862@gmail.com>
2023-04-11 16:45:33 +0700

    update query to database

--- index.js
+++ index.js
@@ -37,17 +37,50 @@ app.post('/mahasiswa', (req, res) =>  {
     const sql = `INSERT INTO mahasiswa (nim, nama, alamat) VALUES (${nim}, '${nama}', '${alamat}')`
     db.query(sql, (err, fields) => {
         console.log(fields)
-    })
-    res.send("ok")
-    // response(200, "ini data", "INI POSTING", res)
+        if (err) response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                id: fields.insertId
+            }
+            response(200, data, "INI POSTING", res)
+        } 
+     })
 })
 
 app.put('/mahasiswa', (req, res) =>  {
-    response(200, "INI UPDATE DATA", res)
+    const {nim, nama, alamat} = req.body
+    const sql = `UPDATE mahasiswa SET nama = '${nama}', alamat = '${alamat}' WHERE nim = ${nim} `
+    db.query(sql, (err, fields) => {
+        console.log(fields)
+        if (err) response(500, "invalid", "error", res)
+        if (fields?.affectedRows) {
+            const data = {
+                isSuccess: fields.affectedRows,
+                message: fields.message
+            }
+            response(200, data, "INI UPDATE DATA", res)
+        } else {
+            response(404, "user not found", "error", res)
+        }
+    })
+    
 })
 
 app.delete('/mahasiswa', (req, res) =>  {
-    response(200, "INI DELETE DATA", res)
+    const {nim} = req.body
+    const sql = `DELETE FROM mahasiswa WHERE nim = ${nim}`
+    db.query(sql, (err, fields) => {
+        if (err) response (500, "invalid", "error", res)
+        if (fields.affectedRows) {
+            const data = {
+                isDelete: fields.affectedRows
+            }
+            response(200, data, "INI DELETE DATA", res)
+        }   else {
+            response(404, "user not found", "error", res)
+        }
+    }) 
 })
 
 app.listen(port, () => {
